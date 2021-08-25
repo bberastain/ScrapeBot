a5ad25362be6ba6c5f3af1f32b142a9c35c065e6
Bruno <bberastain@gmail.com>
2021-08-25 11:33:46 -0700

    database schema has table for search links, each with their own table for listings

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS listings (
+CREATE TABLE IF NOT EXISTS searches (
   id SERIAL PRIMARY KEY,
-  link VARCHAR (300) UNIQUE,
-  text VARCHAR (100),
-  date VARCHAR (100)
+  link VARCHAR (500) UNIQUE,
+  title VARCHAR (50)
 );
-CREATE INDEX link_idx ON listings (link);
+
+-- CREATE TABLE IF NOT EXISTS listings (
+--   id SERIAL PRIMARY KEY,
+--   link VARCHAR (300) UNIQUE,
+--   text VARCHAR (100),
+--   date VARCHAR (100)
+-- );
+-- CREATE INDEX link_idx ON listings (link);
